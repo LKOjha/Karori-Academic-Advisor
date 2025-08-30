18618cc67e5b1f23d11edb5d9ad2fb49974ed62e
Lokesh vishwakarma <lkojha62609@gmail.com>
2025-08-31 00:31:19 +0530

    pages updated

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/services" element={<ServicesPage />} />
-        <Route path="*" element={<NotFound />} />
-
-        
+        <Route path="*" element={<NotFound />} />        
       </Routes>
     </Layout>
   )
